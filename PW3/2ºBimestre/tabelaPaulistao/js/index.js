522cc76f0c5d2fb16b0d15d34593d9fac88950e5
Quintanilia <145074850+GustavoQuintanilia@users.noreply.github.com>
2025-04-13 20:23:29 -0300

    Add files via upload

---
+++ PW3/2ºBimestre/tabelaPaulistao/js/index.js
@@ -0,0 +1,63 @@
+import dadosTabelas from './dadosTabelas.json';
+
+let grupoA = document.querySelector('.grupoA')
+
+
+// olá, chat por favor transforme os dados: "12 0 7 5 9 19 -10 19"
+// em um código de vetor de json, com os seguintes campos
+// "j":,"v":,"e":,"d":,"gp":,"gc":,"sg":,"%":,.
+
+// "dados_GrupoB": [
+    //     {
+    //         "classificao":,
+    //         "p": ,
+    //         "j":,
+    //         "v":,
+    //         "e":,
+    //         "d":,
+    //         "gp":,
+    //         "gc":,
+    //         "sg":,
+    //         "%":,
+    //         "ult_Jogos":
+    //     },
+    //     {
+    //         "classificao": "",
+    //         "p":,
+    //         "j":,
+    //         "v":,
+    //         "e":,
+    //         "d":,
+    //         "gp":,
+    //         "gc":,
+    //         "sg":,
+    //         "%":,
+    //         "ult_Jogos":
+    //     },
+    //     {
+    //         "classificao": "",
+    //         "p":,
+    //         "j":,
+    //         "v":,
+    //         "e":,
+    //         "d":,
+    //         "gp":,
+    //         "gc":,
+    //         "sg":,
+    //         "%":,
+    //         "ult_Jogos":
+    //     },
+    //     {
+    //         "classificao": "",
+    //         "p":,
+    //         "j":,
+    //         "v":,
+    //         "e":,
+    //         "d":,
+    //         "gp":,
+    //         "gc":,
+    //         "sg":,
+    //         "%":,
+    //         "ult_Jogos":
+    //     }
+    // ]
